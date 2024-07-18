e2d961470ba856ee35a642b2844daa5e5091d981
VladislavPixel <pixel.world7@mail.ru>
2024-07-18 17:37:37 +0300

    create columns-editors-routers

--- src/core/end-points.ts
+++ src/core/end-points.ts
@@ -1,2 +1,2 @@
 export const END_POINT_SETTINGS_EDITORS = '/settings-editors';
-export const END_POINT_COLUMNS_EDITORS = '/columns-editors';
+export const END_POINT_COLUMNS_EDITORS = '/columns';
--- src/routes/columns-editors-routers.ts
+++ src/routes/columns-editors-routers.ts
@@ -1,11 +1,16 @@
 import express, { Request, Response } from 'express';
+import { getEndSegmentForUrlDataBase } from '../utils';
+import config from 'config';
+
+import {
+	FIREBASE,
+	END_POINT_COLUMNS_EDITORS
+} from '../core';
 
 const routerColumnsEditors = express.Router({ mergeParams: true });
 
 routerColumnsEditors.post('/', async(req: Request, res: Response) => {
 	const { settingId } = req.body;
-
-	console.log(settingId, '!!!');
 
 	if (!settingId) {
 		res.status(400).send({
@@ -14,6 +19,42 @@ routerColumnsEditors.post('/', async(req: Request, res: Response) => {
 
 		return;
 	}
+
+	const baseUrlColumnsEditors: string = getEndSegmentForUrlDataBase(config.get('urlDB') + END_POINT_COLUMNS_EDITORS);
+
+	if (config.get('nameDB') === FIREBASE) {
+		const responseColumnsEditors = await fetch(baseUrlColumnsEditors, {
+			method: 'GET'
+		});
+
+		const dataColumnsEditors = await responseColumnsEditors.json();
+
+		const arrayIdsColumnsEditors = Object.keys(dataColumnsEditors);
+
+		let arrayColumns = [];
+
+		for (let m = 0; m < arrayIdsColumnsEditors.length; m++) {
+			const idColumnEditor = arrayIdsColumnsEditors[m];
+
+			const columnForEditor = dataColumnsEditors[idColumnEditor];
+
+			if (columnForEditor.settingId === settingId) {
+				arrayColumns.push(columnForEditor);
+			}
+		}
+
+		res.status(200).send({
+			columnsEditor: arrayColumns
+		});
+
+		return;
+	}
+
+	console.log('There must be a data collection trip to another database...');
+
+	res.status(404).send({
+		columnsEditor: null
+	});
 });
 
 export { routerColumnsEditors };
