e7dd69be8d21cc6b473280268a77f27ae7c565dd
VladislavPixel <pixel.world7@mail.ru>
2024-08-28 17:35:15 +0300

    update data account

--- src/schemas/account-sh.ts
+++ src/schemas/account-sh.ts
@@ -1,4 +1,5 @@
 import { Schema } from 'mongoose';
+import { ACCOUNT_COLLECTION_NAME_DB } from '../core';
 
 const types = Schema.Types;
 
@@ -9,7 +10,14 @@ const options = {
 	password: types.String,
 	repeatPassword: types.String,
 	secretPhrase: types.String,
-	acceptanceOfTermsPoliciesAndRules: types.Boolean
+	acceptanceOfTermsPoliciesAndRules: types.Boolean,
+	mountedAccounts: [
+		{
+			type: types.ObjectId,
+			ref: ACCOUNT_COLLECTION_NAME_DB,
+			required: false
+		}
+	]
 };
 
 const schemaAccount = new Schema(options, { _id: true });
